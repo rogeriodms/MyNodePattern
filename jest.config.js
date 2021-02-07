105e123a10656ce8e7337cb4b587b19918a4a53a
rogeriodms <rogeritos260298@gmail.com>
2021-02-07 00:48:05 -0300

    New node parttern

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root:['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
