20d2e4da3372b17b18c5588391b77169db2a9223
Rogerio DMS <rogeritos260298@gmail.com>
2021-02-18 20:40:20 -0300

    chore: add signUp

---
+++ src/Presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import SignUpController from './signup';
+
+describe('sigup controller', () => {
+  test('should return 400 if no name is provide', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com.br',
+        password: 'any_password',
+        passwordConfimation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
