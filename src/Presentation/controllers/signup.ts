d949a927540423e163763b614a274c62af86eef0
Rogerio DMS <rogeritos260298@gmail.com>
2021-02-23 22:10:06 -0300

    refactor: move to duplicate code from helpers

--- src/Presentation/controllers/signup.ts
+++ src/Presentation/controllers/signup.ts
@@ -1,13 +1,11 @@
 import {HttpResponse, HttpRequest } from '../protocols/http'
 import {missingParamError} from '../errors/missing-param-error'
+import {badRequest} from '../helpers/http-helper'
 
 export default class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if(!httpRequest.body.name){
-      return {
-        statusCode: 400,
-        body: new missingParamError('name')
-      };
+      return badRequest(new missingParamError('name'))
     }
     if(!httpRequest.body.email){
       return {
