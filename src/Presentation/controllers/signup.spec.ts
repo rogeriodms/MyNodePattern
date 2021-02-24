1b1c28a70f1c27db4553675af242d444f4740916
Rogerio DMS <rogeritos260298@gmail.com>
2021-02-23 22:20:36 -0300

    test: ensure singUpController returns 400 if no passwordConfimation is provided

--- src/Presentation/controllers/signup.spec.ts
+++ src/Presentation/controllers/signup.spec.ts
@@ -43,4 +43,18 @@ describe('sigup controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new missingParamError('password'));
   });
+
+  test('should return 400 if no email is provide', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com.br',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new missingParamError('passwordConfimation'));
+  });
 });
